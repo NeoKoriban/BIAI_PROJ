5ef5c819dbdc0b09e8d138f7d7d0e642ee0e8b52
Denis <denis.wychowalek@gmail.com>
2014-05-23 13:33:29 +0200

    PlikiProjektowe
    Wszystkie pliki projektowe, które zostały do mnie przesłane.

---
+++ Tablice/Tablice/ManageFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace Tablice
+{
+    class ManageFrame
+    {
+        public ArrayList devices = new ArrayList();
+        public FilterInfoCollection usbCams;
+      //public VideoCaptureDevice cam;
+
+        //Get a list of USB cameras connected to computer
+        public ArrayList getDevices()
+        {
+            usbCams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo camera in usbCams)
+            {
+                devices.Add(camera.Name);
+            }
+            return devices;
+        }
+    }
+}
